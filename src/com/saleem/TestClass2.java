d76a3ea98c141370564605c99578f13e41beecb1
Saleem <saleemtahiri@outlook.com>
2019-01-09 11:41:36 -0600

    This is second test
    Change-Id: I130997ee9346485ac57b3d3732cb5f186665941c

---
+++ src/com/saleem/TestClass2.java
@@ -0,0 +1,15 @@
+package com.saleem;
+
+public class TestClass2 {
+
+	public TestClass2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a test");
+
+	}
+
+}
